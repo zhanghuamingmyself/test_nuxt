f227bdf62a6015265e791f3fb2c67ed38ee11cf3
zhanghm <zhanghm@hithium.cn>
2025-04-30 17:39:59 +0800

    123

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,4 +1,40 @@
 import mysql from 'mysql2/promise';
+import {DataTypes} from "sequelize";
+
+// const { Sequelize } = require('sequelize');
+//
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//     host: process.env.MYSQL_HOST || '172.24.73.24',
+//     dialect: 'mysql',
+// });
+//
+// const User = sequelize.define('User', {
+//     username: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     email: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//         validate: {
+//             isEmail: true,
+//         },
+//     },
+//     // 其他属性和选项
+// });
+//
+// async function createUser() {
+//     await sequelize.sync(); // 同步模型和数据库
+//
+//     const user = await User.create({ username: 'John', email: 'john@example.com' });
+//     console.log(user.toJSON());
+// }
+//
+// createUser().catch(console.error);
+//
+
+
 
 // 创建连接池（提升性能）
 const pool = mysql.createPool({
